2d142b8a96c38522bd42270c0753d0b7312c70aa
sasha_khv1969 <sasha_khv1969@mail.ru>
2024-12-15 20:02:17 +1000

    Update hello_git py. Add about VKS

--- hello_git.py
+++ hello_git.py
@@ -1,3 +1,5 @@
+import os
+
 print('Hello, Git!')
 print('Индексация - это важно')
 print('create New brahch Testing')
@@ -6,3 +8,7 @@ for num in range(10):
         print(num,end=' ')
 
 
+print(f'\nYour current directory: {os.getcwd()}')
+
+s = 'Version Control System'
+print(f'{s} - length is {len(s)} characters')
